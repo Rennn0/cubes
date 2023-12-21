696ee6baf5ba3a7f3ba9d7bfcd4c9bd4f455d952
Luka Danelia <lukadanelia056@gmail.com>
2023-12-20 23:11:06 -0800

    z

--- src/app/lib/task-factory/task-factory.component.ts
+++ src/app/lib/task-factory/task-factory.component.ts
@@ -18,21 +18,37 @@ export class TaskFactoryComponent implements OnInit {
 
   constructor(private _main: MainService) { }
   ngOnInit(): void {
-    this.existingTasks.push({
-      description: "initial",
-      developers: [
-        {
-          name: "Luka",
-          skills: [
-            { name: "C++", K: 9.99 }
-          ],
-          workedOn: [{ name: "some stuff", importance: 1 }]
-        }
-      ],
-      subModules: [{ name: "random module", importance: 1 }],
-      techStack: [{ name: "c++", K: 9.99 }, { name: "Angular", K: 8.88 }],
-      title: "random title"
-    })
+    this.existingTasks.push(
+      {
+        description: "initial",
+        developers: [
+          {
+            name: "Luka",
+            skills: [
+              { name: "C++", K: 9.99 }
+            ],
+            workedOn: [{ name: "some stuff", importance: 1 }]
+          }
+        ],
+        subModules: [{ name: "random module", importance: 1 }],
+        techStack: [{ name: "c++", K: 9.99 }, { name: "Angular", K: 8.88 }],
+        title: "random title"
+      },
+      {
+        description: "initial @",
+        developers: [
+          {
+            name: "Beqa",
+            skills: [
+              { name: "C#", K: 6.66 }
+            ],
+            workedOn: [{ name: "games", importance: 1.3 }]
+          }
+        ],
+        subModules: [{ name: "particle system", importance: 2 }],
+        techStack: [{ name: "c++", K: 9.99 }, { name: "C#", K: 8.88 }],
+        title: "Hushh"
+      })
   }
 
   onTaskModelCreated(newModel: ITaskModel): void {
